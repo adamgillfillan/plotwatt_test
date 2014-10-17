d7b6d8252b5130829ca38349caf1af41a6e63fab
Adam Gillfillan <agillfi@ncsu.edu>
2014-10-17 14:54:15 -0400

    moved initialization arrays to setUp method

--- test_detection_accuracy.py
+++ test_detection_accuracy.py
@@ -4,14 +4,15 @@ import plotwatt
 
 
 class TestDetectionAccuracy(unittest.TestCase):
-    def test_detection_accuracy(self):
+    def setUp(self):
         self.truth = [0.2, 5.0, 1.5]
         self.detected = [0.19, 1.46, 1.8, 1.12]
 
         self.true_positives = [0.19, 1.46]
-        self.false_positives = [1.12, 1.8]
+        self.false_positives = [1.8, 1.12]
         self.false_negatives = [5.0]
 
+    def test_detection_accuracy(self):
         self.assertEquals(plotwatt.detection_accuracy(self.truth, self.detected),
                           [self.true_positives,
                           self.false_positives,

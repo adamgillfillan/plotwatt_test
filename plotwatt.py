04ad7bff480e843c1f41ecc1f4fc2f57797a93fc
Adam Gillfillan <agillfi@ncsu.edu>
2014-10-17 14:07:01 -0400

    added method documentation statement

--- plotwatt.py
+++ plotwatt.py
@@ -1,1 +1,12 @@
 __author__ = 'Adam'
+
+
+def detection_accuracy(truth, detected):
+    """
+        This method aims to find the accuracy of an appliance detection algorithm.
+        It takes 2 lists:
+            truth list of known values.
+            detected list of detected values.
+        The goal is to sort out the true positives, false positives, and false negatives
+    """
+    pass

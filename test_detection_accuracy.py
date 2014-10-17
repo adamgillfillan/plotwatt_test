44de9b7fb31af5a0176fdf115cea1efe54ac552a
Adam Gillfillan <agillfi@ncsu.edu>
2014-10-17 14:13:36 -0400

    added unittest for detection accuracy function

---
+++ test_detection_accuracy.py
@@ -0,0 +1,18 @@
+__author__ = 'Adam'
+import unittest
+import plotwatt
+
+
+class TestDetectionAccuracy(unittest.TestCase):
+    def test_detection_accuracy(self):
+        self.truth = [0.2, 5.0, 1.5]
+        self.detected = [0.19, 1.46, 1.8, 1.12]
+
+        self.true_positives = [0.19, 1.46]
+        self.false_positives = [1.12, 1.8]
+        self.false_negatives = [5.0]
+
+        self.assertEquals(plotwatt.detection_accuracy(self.truth, self.detected),
+                          self.true_positives,
+                          self.false_positives,
+                          self.false_negatives)
